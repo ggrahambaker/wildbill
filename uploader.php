7e5b260c3ee23e612cb389cf419761eaa6468273
Zeke Rosenberg <zrosenberg@pugetsound.edu>
2015-05-03 21:42:14 -0700

    added some bootstrap to uploader

--- uploader.php
+++ uploader.php
@@ -69,7 +69,12 @@ if(isset($_POST["logIn"])){
 <html>
 
 <head>
+	<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Orwell Upload</title>
+	<link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="wb.css">
 	</head>
 
 <body>
